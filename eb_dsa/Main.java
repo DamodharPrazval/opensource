01bacd2d7099fbb53ce9b91d14aa8f8b92f83514
DamodharPrazval <dhamodharprazwal@gmail.com>
2024-11-12 10:35:15 +0530

    Create Main.java

---
+++ eb_dsa/Main.java
@@ -0,0 +1,12 @@
+#import java.util.Scanner;
+class main{
+  public static void main(String []args) {
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    if (x%2==0) {
+      System.out.ptintln("EVEN");
+    }else{
+      System.out.println("ODD");
+    }
+  }
+}
